fc0f2e4af4b33bc1e0bbc72a26b7293711dbedfc
David Gemperline <dcgemperline@gmail.com>
2016-05-22 16:06:33 -0500

    wrote new function to estimate fold change and seperate out kde plots seperately

--- Seaborn_Graphing/Seaborn_Graphing.py
+++ Seaborn_Graphing/Seaborn_Graphing.py
@@ -1,7 +1,9 @@
 import seaborn as sns
 import matplotlib as mpl
 import numpy as np
+from decimal import *
 import DCG_Utilities as dcgutils
+import scipy
 ## Allows for easy text manipulation in illustrator by changing this
 mpl.rcParams['pdf.fonttype'] = 42
 
@@ -251,8 +253,90 @@ class PlottingFunctions:
            with open(outputfilename, "wt") as out_file:
                out_file.write("No Significant Interactors")
 
+    def GenerateKDEAlonePlots(self, outputFileName):
+        ##Read in Data
+        df = self._LFQDataFrame
+        ##Read in Curve
+        df2 = self._CurveDataFrame
+        ## NOTES
+        ## replace NaN in subcomplex labels with unlabeled so that it plots correctly, and then setup color
+        ## hue so that it turns grey
+        ## 
+        df = df.replace({'SubComplexAnnotation': { np.NaN : 'Unlabeled'}})
+
+
+        ## Think about doing a 1D density plot on the axes instead of a contour plot
+
+        # Subset the Volcano Plot by ProteasomeSubComplex
+
+        RP = df.loc[df['SubComplexAnnotation'] == 'RP']
+        CP = df.loc[df['SubComplexAnnotation'] == 'CP']
+        PIPS_CP = df.loc[df['SubComplexAnnotation'] == 'PIP_CP']
+        PIPS_RP = df.loc[df['SubComplexAnnotation'] == 'PIP_RP']
+        PIP = df.loc[df['SubComplexAnnotation'] == 'PIP']
+
+        ## Setup XKCD Colors
+        colors = ["cool grey", "windows blue", "pale red", "medium green", "orange", "black"]
+        ## Parse Colors
+        colors = sns.xkcd_palette(colors);
+
+        ##SetupCusotmPalleteDictionary
+        ##if self._LabelType=="AllPips":
+        ##    custompalette=dict(Unlabeled=colors[0], RP=colors[1], CP=colors[2], PIP=colors[3], PIP_CP=colors[3], PIP_RP=colors[3])
+        ##if self._LabelType=="CPPips":
+        ##    custompalette=dict(Unlabeled=colors[0], RP=colors[1], CP=colors[2], PIP=colors[3], PIP_CP=colors[3], PIP_RP=colors[0])
+        ##if self._LabelType=="RPPips":
+        ##    custompalette=dict(Unlabeled=colors[0], RP=colors[1], CP=colors[2], PIP=colors[3], PIP_CP=colors[0], PIP_RP=colors[3])
+        ##if self._LabelType=="ECM29":
+        ##    custompalette=dict(Unlabeled=colors[0], RP=colors[1], CP=colors[2], PIP=colors[3], PIP_CP=colors[0], PIP_RP=colors[0])
+        ##if self._LabelType=="CPRP":
+        ##    custompalette=dict(Unlabeled=colors[0], RP=colors[1], CP=colors[2], PIP=colors[0], PIP_CP=colors[0], PIP_RP=colors[0])
+    
+        ##Rick's Palette
+        custompalette=dict(Unlabeled=colors[0], RP=colors[1], CP=colors[2], PIP=colors[5], PIP_CP=colors[3], PIP_RP=colors[4])
+
+        red = sns.color_palette("Reds")[-2]
+        blue = sns.color_palette("Blues")[-2]
+
+        ## Setup Seaborn facetgrid with lmplot (no line)
+        plt.gcf().clear()
+        kdeax = sns.kdeplot(CP['Difference'], label='CP Density', color =red, bw=0.5, shade=True, shade_lowest=False, alpha = 0.5)
+        kdemedianset1 = self.KDEMedian(kdeax)
+        plt.gcf().clear()
+        kdeax2 = sns.kdeplot(RP['Difference'], label='RP Density', color =blue, bw=0.5, shade=True, shade_lowest=False, alpha = 0.5)
+        kdemedianset2 = self.KDEMedian(kdeax2)
+        plt.gcf().clear()
+
+        kdeax = sns.kdeplot(CP['Difference'], label='CP Density', color =red, bw=0.5, shade=True, shade_lowest=False, alpha = 0.5)
+        kdeax2 = sns.kdeplot(RP['Difference'], label='RP Density', color =blue, bw=0.5, shade=True, shade_lowest=False, alpha = 0.5, ax =kdeax)
+
+        ## Setup Legend
+        #facetgrid.fig.get_axes()[0].legend(loc='upper left', title = 'Key')
+
+        ## Setup Axes and axes limit
+        myaxes = kdeax.axes
+        myaxes.set_ylim(0,1)
+        myaxes.set_xlim(-2,13)
+        
         
 
+        plt.vlines(kdemedianset1[0], 0, kdemedianset1[1], color=red)
+        plt.vlines(kdemedianset2[0], 0, kdemedianset2[1], color=blue)
+        foldchange = np.abs(kdemedianset1[0] - kdemedianset2[0])
+        foldchange = np.exp2(foldchange)
+        foldchange = round(Decimal(foldchange), 2)
+        xoffset = -5;
+        yoffset = -0.2
+        xposition = 13
+        yposition = 1
+        labeltoAdd = "Fold Change:"
+        labeltoAdd += str(foldchange)
+        plt.text(xposition+xoffset,yposition+yoffset, labeltoAdd, size=16)
+
+        ## Save Figure
+        plt.savefig(outputFileName + ".pdf");
+        plt.savefig(outputFileName + ".png");    
+        
 
     def LabelFigure(self, dataframe, ax, offset, colors):
         for index, row in dataframe.iterrows():
@@ -260,6 +344,17 @@ class PlottingFunctions:
             yposition=row['-Log(P-value)']
             labeltoAdd=row['ProteasomeAnnotation']
             ax.text(xposition+offset,yposition+offset, labeltoAdd, size=16, color = colors[5])
+    
+    def KDEMedian(self, kdeplot):
+        x,y = kdeplot.get_lines()[0].get_data()
+        cdf = scipy.integrate.cumtrapz(y, x, initial=0)
+        nearest_05 = np.abs(cdf-0.5).argmin()
+
+        x_median = x[nearest_05]
+        y_median = y[nearest_05]
+
+        return [x_median, y_median]
+            
     def Thougts(self):
     #LabelList = ["PAG1_Minus_vs_Col0_Minus",
     #    "PAG1_Minus_vs_PAG1_Plus",
@@ -301,6 +396,7 @@ fileAndStyle3 = {'RPT4b_Plus_RPT4b_Minus' : 'ECM29', 'RPT4a_Plus_RPT4a_Minus' : 
 for name, labelstyle in fileAndStyle.items():
     plot = PlottingFunctions(name +".txt", name + "_Curve.txt", labelstyle)
     plot.GenerateFigure(name, shouldLabel=True)
+    plot.GenerateKDEAlonePlots(name + "_kde.txt")
     plot.GetSignificantInteractorsThatAreNotProteasomeSubunits(name + "_Significant_Interactors.txt")
 
 for name, labelstyle in fileAndStyle2.items():
